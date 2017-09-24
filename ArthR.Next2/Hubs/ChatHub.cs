165143c317605cf69411e6799cf703d2c69c1be0
Arthur Gorr <art.gorr@gmail.com>
2017-09-23 21:57:14 -0500

    Barebones  hub

---
+++ ArthR.Next2/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ArthR.Next.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string message)
+        {
+            Clients.All.InvokeAsync("Send", message);
+        }
+    }
+}
